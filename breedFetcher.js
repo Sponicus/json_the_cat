d5054b02358f19dbd61b0e5ca6c7163931e17870
Andrew <andrew.spon@gmail.com>
2021-11-04 21:53:21 +0000

    clean up

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,15 @@
 const request = require('request');
-// const prompt = process.argv.slice(2)[0];
-const fetchBreedDescription = function(breedName, callback) { 
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-        // console.log('Oh no! An error has occurred!',error);
-      callback('Oh no! An error has occurred!',null) //If we have error, we use the CAllback user provided to pass the error back.
+      callback('Oh no! An error has occurred!',null);
     } else {
       const data = JSON.parse(body);
       const breed = data[0];
       if (breed) {
-        // console.log(breed.description);
-        callback(null, breed.description)
+        callback(null, breed.description);
       } else {
-        // console.log(`Failed to find ${breedName}`);
-        callback(`Failed to find ${breedName}`, null)
+        callback(`Failed to find ${breedName}`, null);
       }
     }
    
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {fetchBreedDescription} = require('./breedFetcher');
 const prompt = process.argv.slice(2)[0];
 
-fetchBreedDescription(prompt, (error, desc) => { 
+fetchBreedDescription(prompt, (error, desc) => {
   if (error) {
     console.log('Index - Oh no! An error has occurred!', error);
   } else {
