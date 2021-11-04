8f12f8fb13938e0e22b0f56bee787f57f383b207
Andrew <andrew.spon@gmail.com>
2021-11-04 20:30:03 +0000

    successful return data

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,8 @@
 const request = require('request');
-const prompt = process.argv
-console.log(prompt);
-request(`https://api.thecatapi.com/v1/breeds/search?q=Siberian`, (error, response, body) => {
-  // console.log('error:', error); 
+const prompt = process.argv.slice(2)[0]
+// console.log(prompt);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${prompt}`, (error, response, body) => {
+  // console.log('error:', error);
   // console.log('statusCode:', response && response.statusCode); 
   // console.log('body:', body);
   const data = JSON.parse(body);
