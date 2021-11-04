be6dbc6dbff9cc311ff5fcab9219bf5df9164599
Andrew <andrew.spon@gmail.com>
2021-11-04 20:57:59 +0000

    eslint code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,9 @@
 const request = require('request');
-const prompt = process.argv.slice(2)[0]
+const prompt = process.argv.slice(2)[0];
 request(`https://api.thecatapi.com/v1/breeds/search?q=${prompt}`, (error, response, body) => {
-  if (error) {console.log('Oh no! An error has occurred!',error)}
+  if (error) {
+    console.log('Oh no! An error has occurred!',error);
+  }
   const data = JSON.parse(body);
   const breed = data[0];
   if (breed) {
