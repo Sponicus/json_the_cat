00b898caec3148ca3eebcbc47896b04e678f403a
Andrew <andrew.spon@gmail.com>
2021-11-04 20:55:45 +0000

    implement error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,7 +5,14 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${prompt}`, (error, respon
   // console.log('error:', error);
   // console.log('statusCode:', response && response.statusCode); 
   // console.log('body:', body);
+  if (error) {console.log('Oh no! An error has occurred!',error)}
   const data = JSON.parse(body);
-  console.log(data);
+  // console.log(data);
+  const breed = data[0];
+  if (breed) {
+    console.log(breed.description);
+  } else {
+    console.log(`Failed to find ${prompt}`);
+  }
   // console.log(typeof data);
 });
